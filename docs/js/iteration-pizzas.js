377525ee37a436f40e75c17213f3f0ed8b008967
Josiel Faria <josiel@farmaservice.com.br>
2019-08-13 17:54:04 -0300

    Alteração do layout das iterações com as promos e trios do site, ajustes no css e imagens novas para botões

--- docs/js/iteration-pizzas.js
+++ docs/js/iteration-pizzas.js
@@ -1,3 +1,9 @@
+
+//
+// PIZZAS INTERATIONS
+//
+
+
 (function() {
   var x = document.getElementById("imgPizza1");
   x.addEventListener("mouseover", mouseover);
@@ -53,5 +59,85 @@
     var x = document.getElementsByClassName("w3-display-topleft");
     x[2].style.width = "50%";
     x[2].style.borderTopRightRadius = "0px";
+  }
+})();
+
+(function() {
+  var x = document.getElementById("imgPizza4");
+  x.addEventListener("mouseover", mouseover);
+  x.addEventListener("mouseout", ouseout);
+
+  function mouseover() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[3].style.width = "100%";
+    x[3].style.borderTopRightRadius = "2px";
+  }
+
+  function ouseout() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[3].style.width = "50%";
+    x[3].style.borderTopRightRadius = "0px";
+  }
+})();
+
+
+
+
+
+//
+// HAMBURGUERE INTERATIONS
+//
+
+(function() {
+  var x = document.getElementById("imgTrio1");
+  x.addEventListener("mouseover", mouseover);
+  x.addEventListener("mouseout", ouseout);
+
+  function mouseover() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[4].style.width = "100%";
+    x[4].style.borderTopRightRadius = "2px";
+  }
+
+  function ouseout() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[4].style.width = "50%";
+    x[4].style.borderTopRightRadius = "0px";
+  }
+})();
+
+(function() {
+  var x = document.getElementById("imgTrio2");
+  x.addEventListener("mouseover", mouseover);
+  x.addEventListener("mouseout", ouseout);
+
+  function mouseover() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[5].style.width = "100%";
+    x[5].style.borderTopRightRadius = "2px";
+  }
+
+  function ouseout() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[5].style.width = "50%";
+    x[5].style.borderTopRightRadius = "0px";
+  }
+})();
+
+(function() {
+  var x = document.getElementById("imgTrio3");
+  x.addEventListener("mouseover", mouseover);
+  x.addEventListener("mouseout", ouseout);
+
+  function mouseover() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[6].style.width = "100%";
+    x[6].style.borderTopRightRadius = "2px";
+  }
+
+  function ouseout() {
+    var x = document.getElementsByClassName("w3-display-topleft");
+    x[6].style.width = "50%";
+    x[6].style.borderTopRightRadius = "0px";
   }
 })();
