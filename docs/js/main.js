fa54f75c0e08d257a9af09b1b69f903b15526c63
Josiel Faria <josiel@farmaservice.com.br>
2019-09-09 17:31:53 -0300

    Ajustes nos pratos executivos e layout do menu

--- docs/js/main.js
+++ docs/js/main.js
@@ -95,3 +95,30 @@ function hideMenu(){
   document.getElementById('navSmall').classList.remove("w3-show");
   console.log('asjdios')
 }
+
+
+// 
+// DROPDOWN MENU
+// 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  console.log('asrfa')
+}
+function myFunctionTwo() {
+  document.getElementById("myDropdownTwo").classList.toggle("show");
+  console.log('asrfa')
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
